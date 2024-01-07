be667f877eeabb6acb767b8d66c8793428ce3dde
AlickSadler <alicksadler@gmail.com>
2024-01-07 04:44:35 +0200

    'new'

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -103,97 +103,98 @@ const Game: FC = () => {
 
 	return (
 		<>
-			{isLandscape && (
-				<>
-					<Helmet>
-						<title>Game</title>
-						{assets.map((imageName, index) => (
-							<link
-								key={index}
-								rel='preload'
-								as='image'
-								href={`./assets/cards/${imageName}.svg`}
-							/>
-						))}
-					</Helmet>
+			<Helmet>
+				<title>Game</title>
+				{assets.map((imageName, index) => (
+					<link
+						key={index}
+						rel='preload'
+						as='image'
+						href={`./assets/cards/${imageName}.svg`}
+					/>
+				))}
+			</Helmet>
 
-					{loading && (
-						<div className='flex-center'>
-							<Spinner />
-						</div>
-					)}
-					{update && (
-						<div
-							className={styles.page}
-							style={{ transition: '3s', opacity: opacity }}>
-							<GameHeader
-								isFullScreen={isFullScreen}
-								handleFullScreen={handleFullScreen}
-							/>
-							<Table
-								roomState={roomState}
-								setRoomState={setRoomState}
-								ready={ready}
-								setReady={setReady}
-								setLoading={setLoading}
-								mePlayer={mePlayer}
-								setMePlayer={setMePlayer}
-								setUpdate={setUpdate}
-								setOpacity={setOpacity}
-								tableRef={tableRef}
-							/>
-							<GameFooter
-								isEnable={
-									mePlayer.state === 'move' && roomState.state === 'bidding'
-								}
-								isReady={ready}
-								joinTax={Number(roomState.join_tax)}
-								maxBid={Number(roomState.max_bid)}
-								bid={Number(roomState.bid)}
-								readyHandler={readyHandler}
-							/>
-						</div>
-					)}
-					{gameAction.state === 'defeat' && (
-						<ModalAfterGame
-							title='Ви програли'
-							message='Сума програшу:'
-							isWin={false}
-							sum={mePlayer.full_bid}
-							onClick={() => {
-								dispatch(setGameAction({ state: '' }));
-							}}
-						/>
-					)}
-					{gameAction.state === 'won' && (
-						<ModalAfterGame
-							title='Ви виграли'
-							message='Сума виграшу:'
-							isWin={true}
-							sum={roomState.bank * 0.95}
-							onClick={() => {
-								setRoomResultState({} as PublicRoomResponce);
-								dispatch(setGameAction({ state: '' }));
-							}}
-						/>
-					)}
-					{gameAction.state === 'room-not-found' && (
-						<ModalAfterGame
-							title='Повідомлення'
-							message="З'єднання з кімнатою було втрачене. Зайдіть в другу кімнату або звяжіться з нашими менеджерами."
-							value='На головну'
-							isHide={false}
-							onClick={() => {
-								dispatch(setGameAction({ state: '' }));
-								navigate('/');
-								location.reload();
-							}}
-						/>
-					)}
-				</>
+			{loading && (
+				<div className='flex-center'>
+					<Spinner />
+				</div>
 			)}
+			{update && (
+				<div
+					className={styles.page}
+					style={{ transition: '3s', opacity: opacity }}>
+					<GameHeader
+						isFullScreen={isFullScreen}
+						handleFullScreen={handleFullScreen}
+					/>
+					<Table
+						roomState={roomState}
+						setRoomState={setRoomState}
+						ready={ready}
+						setReady={setReady}
+						setLoading={setLoading}
+						mePlayer={mePlayer}
+						setMePlayer={setMePlayer}
+						setUpdate={setUpdate}
+						setOpacity={setOpacity}
+						tableRef={tableRef}
+					/>
+					<GameFooter
+						isEnable={
+							mePlayer.state === 'move' && roomState.state === 'bidding'
+						}
+						isReady={ready}
+						joinTax={Number(roomState.join_tax)}
+						maxBid={Number(roomState.max_bid)}
+						bid={Number(roomState.bid)}
+						readyHandler={readyHandler}
+					/>
+				</div>
+			)}
+			{gameAction.state === 'defeat' && (
+				<ModalAfterGame
+					title='Ви програли'
+					message='Сума програшу:'
+					isWin={false}
+					sum={mePlayer.full_bid}
+					onClick={() => {
+						dispatch(setGameAction({ state: '' }));
+					}}
+				/>
+			)}
+			{gameAction.state === 'won' && (
+				<ModalAfterGame
+					title='Ви виграли'
+					message='Сума виграшу:'
+					isWin={true}
+					sum={roomState.bank * 0.95}
+					onClick={() => {
+						setRoomResultState({} as PublicRoomResponce);
+						dispatch(setGameAction({ state: '' }));
+					}}
+				/>
+			)}
+			{gameAction.state === 'room-not-found' && (
+				<ModalAfterGame
+					title='Повідомлення'
+					message="З'єднання з кімнатою було втрачене. Зайдіть в другу кімнату або звяжіться з нашими менеджерами."
+					value='На головну'
+					isHide={false}
+					onClick={() => {
+						dispatch(setGameAction({ state: '' }));
+						navigate('/');
+						location.reload();
+					}}
+				/>
+			)}
+
 			{!isLandscape && (
-				<div className={styles.flex}>Гра не підтримує портретний режим</div>
+				<div
+					style={{ position: 'fixed', top: 0, left: 0, zIndex: 10000 }}
+					className={styles.flex}>
+					Гра не підтримує портретний режим
+				</div>
 			)}
 		</>
 	);
