91543540c2febcc9eca5745cbc533a26e4b37c44
AlickSadler <alicksadler@gmail.com>
2024-01-08 12:41:02 +0200

    'new'

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -13,10 +13,10 @@ interface ITreeCards {
 }
 
 const TreeCards: FC<ITreeCards> = ({ cards, number, visible, style }) => {
-	const type = (cards?.length === 3 && 'jpg') || 'jpg';
+	const type = (cards?.length === 3 && 'svg') || 'jpg';
 	const cardsItem = cards && cards.length ? cards : ['fb', 'fb', 'fb'];
 
-	const baseCardsUrl = './assets/cards-load';
+	const baseCardsUrl = './assets/cards';
 
 	const cssStyle = (style && {
 		...style,
