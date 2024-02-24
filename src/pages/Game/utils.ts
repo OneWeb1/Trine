7dbd37f4cd75fc2e209bb458502beafedf77baf6
AlickSadler <alicksadler@gmail.com>
2024-02-24 10:52:23 +0200

    Changed the logic of adapting the table to the screen size

--- src/pages/Game/utils.ts
+++ src/pages/Game/utils.ts
@@ -74,7 +74,6 @@ const resizeHandler = (tableRef: RefObject<HTMLDivElement>) => {
 		table.style.transform = `scale(${scale}) perspective(900px) rotateX(${custDeg})`;
 	} else {
 		const horZoom = window.innerWidth / portraitTableWidth;
-		const vertZoom = window.innerHeight / landscapeTableHeight;
 		const vertPadding =
 			window.innerHeight > 800 ? 300 : window.innerHeight > 600 ? 180 : 250;
 		const horPadding = 90 * horZoom;
@@ -89,44 +88,6 @@ const resizeHandler = (tableRef: RefObject<HTMLDivElement>) => {
 		const custDeg = '0deg';
 		table.style.transform = `scale(${scale}) perspective(900px) rotateX(${custDeg})`;
 	}
-
-	// const screens = [
-	// 	[1600, 1200, 0.85],
-	// 	[1200, 1100, 0.8],
-	// 	[1100, 1000, 0.8],
-	// 	[1000, 900, 0.55],
-	// 	[900, 800, 0.5],
-	// 	[800, 700, 0.45],
-	// 	[700, 600, 0.42],
-	// 	[600, 500, 0.5],
-	// 	[500, 400, 0.5],
-	// 	[400, 300, 0.5],
-	// 	[300, 200, 0.38],
-	// 	[260, 160, 0.34],
-	// ];
-	// let deg = 0;
-	// if (deg) deg * deg;
-	// screens.forEach(screen => {
-	// 	const [max, min, zoom, d] = screen;
-	// 	if (screenWidth <= max && screenWidth >= min) {
-	// 		const angle = d || 0;
-	// 		scale = zoom;
-	// 		deg = angle;
-	// 	}
-	// });
-
-	// if (screenWidth <= 600 && window.innerHeight < 670) {
-	// 	table.style.transform = `scale(${0.35}) perspective(900px) rotateX(${custDeg})`;
-	// } else if (screenWidth <= 600 && window.innerHeight < 750) {
-	// 	table.style.transform = `scale(${0.45}) perspective(900px) rotateX(${custDeg})`;
-	// } else if (window.innerHeight < 300 && window.innerWidth > 700) {
-	// 	table.style.transform = `scale(${0.32}) perspective(900px) rotateX(${custDeg}) translateY(-121px)`;
-	// } else if (window.innerHeight < 301 && window.innerWidth < 700) {
-	// 	table.style.transform = `scale(${0.32}) perspective(900px) rotateX(${custDeg}) translateY(0px)`;
-	// } else if (window.innerHeight < 420 && window.innerHeight < 1200) {
-	// 	table.style.transform = `scale(${0.45}) perspective(900px) rotateX(${custDeg})`;
-	// } else
-	// 	table.style.transform = `scale(${scale}) perspective(900px) rotateX(${custDeg})`;
 };
 
 const getRoomsIndexPosition = (length: number): number[] => {
