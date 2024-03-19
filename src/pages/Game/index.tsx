7e24699162bcf433fe138e12cf7aff28e3384ebc
AlickSadler <alicksadler@gmail.com>
2024-03-19 23:40:13 +0200

    Add button for copy room state

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,5 +1,4 @@
 import { FC, useState, useRef, useEffect } from 'react';
-import { LuCopy } from 'react-icons/lu';
 
 import { useNavigate, useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
