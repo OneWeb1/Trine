7cb061476c7b158d1a8c020831104b573c46f27d
AlickSadler <alicksadler@gmail.com>
2024-06-11 09:45:44 +0300

    Add: Added a statistics tab for wheel fortune

--- src/services/WheelFortuneService.ts
+++ src/services/WheelFortuneService.ts
@@ -1,6 +1,6 @@
 import { AxiosResponse } from 'axios';
 import {
-	WheelFortuneHistoryPaginationResponse,
+	// WheelFortuneHistoryPaginationResponse,
 	WheelFortuneHistoryResponse,
 	WheelFortuneResultResponse,
 	WheelFortuneStateResponse,
